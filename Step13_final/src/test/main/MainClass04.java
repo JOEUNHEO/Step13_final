1510f929bb5126e90ffaa63305bddbe5af3c5de0
Heojoeun <joeun424@gmail.com>
2017-11-22 17:33:36 +0900

    final 완료!

---
+++ Step13_final/src/test/main/MainClass04.java
@@ -0,0 +1,14 @@
+package test.main;
+
+import test.mypac.GreetHelper;
+
+public class MainClass04 {
+	public static void main(String[] args) {
+		//일본어 인사말을 출력하고 싶다면?
+		System.out.println(GreetHelper.JPN);
+		//중국어 인사말을 출력하고 싶다면?
+		System.out.println(GreetHelper.CHN);
+		
+		//누군가 정의해 놓은 static final 상수를 쓸 일이 많다!
+	}
+}
---
+++ Step13_final/src/test/main/MainClass05.java
@@ -0,0 +1,8 @@
+package test.main;
+
+public class MainClass05 {
+	//클래스를 만들 때, final 예약어를 붙이면 자식을 가질 수가 없다. (상속 X) - 종단 클래스
+	public final class Ss{
+		
+	}
+}
